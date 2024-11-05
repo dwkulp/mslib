0ef20fd231b931508ea7448dfe129a86fc832027
dwkulp <dwkulp@gmail.com>
2024-11-05 22:37:07 +0000

    New programs added

--- myProgs/dwkulp/dwkulp.mk
+++ myProgs/dwkulp/dwkulp.mk
@@ -4,17 +4,29 @@
 MYSOURCE  = DistanceMatrix DistanceMatrixDatabase DistanceMatrixResult ManageDistanceMatrixResults MatrixWindow
 
 MYPROGS   = discoverMotif generateRotamerLibrary getTripletCaMeasurements superRotamerExtraction writeShiftedRamaStats \
-            buildInverseRotamers findCisPepBonds createVectorHash splitIntoChains getAAStats searchForFusions generateBioUnit \
+            buildInverseRotamers findCisPepBonds createVectorHash splitIntoChains getAAStats  searchForNanos generateBioUnit \
             findPositionsWithRotamers dualLoopSearch insertSelectionIntoTemplate buildBinaryDMs multiSearchDM compareStructures \
-	    querySeqCons calcSasaAll designLinearSegment checkRMS_EOD conformationalSampling glycineSearch clashCheck setupRemodelFusions \
-	    headless_resfiles circularPermutant clusterDiagnols checkCloseTerminii getVectorPairs findPositionsWithRotamersRon \
+	    querySeqCons calcSasaAll designLinearSegment conformationalSampling glycineSearch clashCheck setupRemodelFusions \
+	    circularPermutant clusterDiagnols checkCloseTerminii getVectorPairs  \
 	    setupRosettaMSA designCheck domainSasa getMeasurement getPairEnergy metalRotamers patchAnalysis compareRosettaModels setupRosettaFixbb \
-	    getConditionalPhiPsi refinePotentialFusions refinePotentialFusions_single createTertFragDB buildRotamers getLoopLengths resurfaceSaltBridges findMotifMSA \
-	    fitCoiledCoil addResiduesMinClash querySeqConsMulti alignList cluster getDistanceToHelixAxis calcSasa structureSelectedMSA transplantResidues \
-	    analyzeSurfacePositionsMSA findDisulfides findTermini measureDisulfides multiHelixMatch analyzeRosettaModels getGlycanDihedrals alignByFrame alignVirus getRosettaScores aaScan \
-            makeChimeraMSA genIdealHelix fuseHelix getAnglesPDBFrags reportGlycanData getSphericalBounds simpleNanoparticleModeling convertPDBtoCIF getGlycanDihedrals betaDecider
-#calcSasaInterface
-
+	    refinePotentialFusions buildRotamers resurfaceSaltBridges findMotifMSA \
+	    findDisulfides  getGlycanDihedrals alignByFrame aaScan \
+        simpleNanoparticleModeling convertPDBtoCIF getGlycanDihedrals betaDecider betaAnalysis barrelDecider_v7 alignByFrame addCodonFreq moveToOrigin \
+		rigidBodyGeneration transformSelections getSSE backRubApp getDeltaDistSASA 
+#  alignList cluster getDistanceToHelixAxis  calcSasa  	    structureSelectedMSA transplantResidues \
+# analyzeSurfacePositionsMSA  findTermini  measureDisulfides multiHelixMatch  analyzeRosettaModels alignVirus getRosettaScores 
+#calcSasaInterface makeChimeraMSA genIdealHelix fuseHelix 
+#querySeqConsMulti getAnglesPDBFrags reportGlycanData getSphericalBounds 
+#checkRMS_EOD
+#findPositionsWithRotamersRon
+#searchForFusions
+#getConditionalPhiPsi
+#createTertFragDB
+#getLoopLengths
+#fitCoiledCoil
+#addResiduesMinClash 
+# refinePotentialFusions_single
+#headless_resfiles
 MYHEADERS = 
 
 MYDIR=myProgs/dwkulp
